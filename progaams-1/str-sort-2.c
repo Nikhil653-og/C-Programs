42418c8c6809b8a21335adf0e857054fb134f0e4
Nikhil653-og <nikhiledwin2023@gmail.com>
2025-03-04 06:09:36 +0530

    fixed loop issue

--- progaams-1/str-sort-2.c
+++ progaams-1/str-sort-2.c
@@ -1,29 +1,23 @@
-
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-    int i, j, len, n = 3;
-    char str1[n][25], str2[15], temp[25];
-    printf("Enter strings:");
-    for (i = 0; i < n; i++)
-    {
-        fgets(str1[i], 25, stdin);
-    }
+    int i, j, k, len, n = 3;
+    char s1[3][15] = {"hello", "cool", "march"}, temp[15];
     for (i = 0; i < n - 1; i++)
     {
-        for (j = i + 1; j < n - 1; j++)
+        for (j = 0; j < n - i - 1; j++)
         {
-            if (strcmp(str1[j], str1[j + 1]) > 0)
+            if (strcmp(s1[j], s1[j + 1]) > 0)
             {
-                strcpy(temp, str1[j]);
-                strcpy(str1[j], str1[j + 1]);
-                strcpy(str1[j + 1], temp);
+                strcpy(temp, s1[j]);
+                strcpy(s1[j], s1[j + 1]);
+                strcpy(s1[j + 1], temp);
             }
         }
     }
-    for (i = 0; i < n; i++)
+    for (i = 0; i < 3; i++)
     {
-        printf("%s \n", str1[i]);
+        printf("%s\n", s1[i]);
     }
 }
